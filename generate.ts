c8ec18c1fe90fc275e26b58b26baf296c6622172
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2025-03-31 17:30:49 -0700

    Install package oazapfts

---
+++ generate.ts
@@ -0,0 +1,5 @@
+import { generateSource } from "oazapfts";
+
+// TODO: Generate client library from OpenAPI schema.
+// https://github.com/oazapfts/oazapfts/blob/89944fbb0383f0eaf1d33d4a7f99bd0fe7c18eca/packages/codegen/src/index.ts#L33
+//
