032a403fcbe6a91a36dc1ca15629f3aa8284bf7f
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2025-04-01 21:38:33 -0700

    wip

--- generate.ts
+++ generate.ts
@@ -2,6 +2,7 @@ import { generateSource } from "oazapfts";
 import { server } from "./server.ts";
 
 if (import.meta.main) {
+  // TODO: Transform TypeScript to JavaScript.
   const result = await generateSource(
     server.specification as unknown as string,
     { optimistic: true },
